699299e3da136cbe9a2f1abb2f1822e2a1dfb843
helmirmili3 <helmi.rmili@isimg.com>
2025-02-20 23:34:51 +0100

    use .env for variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,17 +8,17 @@ import {
 } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDyN-qhJrq3rztyxiRGLk4l9KP9KC2ADNs",
-  authDomain: "sparexchange-323c8.firebaseapp.com",
-  databaseURL:
-    "https://sparexchange-323c8-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "sparexchange-323c8",
-  storageBucket: "sparexchange-323c8.firebasestorage.app",
-  messagingSenderId: "489377059750",
-  appId: "1:489377059750:web:da633a2da0b774d3cc7567",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
